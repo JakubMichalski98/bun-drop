d31b801fcf28701e1ebfecefe6339d81048ae642
serein3 <jakubmichalski9818@gmail.com>
2023-05-30 20:15:43 +0200

    Some progress on navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
 import Styles from './Navbar.module.css'
 import { Link, NavLink } from 'react-router-dom';
+import { useState } from 'react';
 import {AiOutlineShoppingCart} from 'react-icons/ai';
+import {RxHamburgerMenu, RxCross2} from 'react-icons/rx'
 
 function Navbar() {
+
+    const [isMenuClicked, setIsMenuClicked] = useState(true);
+
+    function toggleMenuIcon()
+    {
+        setIsMenuClicked(!isMenuClicked);
+    }
+
     return ( 
         <nav className={Styles.navbar}>
             <Link to='/'>
                 <img src='/src\images\logo-color.png'/>
             </Link>
-                <ul className={Styles.navmenu}>
+                <ul className={Styles.mobileicons}>
+                    <li>
+                        <div onClick={toggleMenuIcon}>
+                            {!isMenuClicked ? (
+                                <RxHamburgerMenu className={Styles.hamburgermenu}/>
+                            ) : 
+                            (
+                                <RxCross2 className={Styles.hamburgermenu}/> 
+                            )}
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <NavLink to='/cart'>
+                                <AiOutlineShoppingCart className={Styles.carticon}/>
+                            </NavLink>
+                        </div>
+                    </li>
+                </ul>
+                <ul className={isMenuClicked? Styles.mobilemenu : Styles.navmenu}>
                     <li>
                         <NavLink to='/'>
                             HOME
@@ -22,13 +51,16 @@ function Navbar() {
                     </li>
                     <li>
                         <NavLink to='/signin'>
-                            SIGN IN
+                            SIGN&nbsp;IN
                         </NavLink>
                     </li>
                 </ul>
+
                 <div className={Styles.cart}>
-                    <AiOutlineShoppingCart className={Styles.carticon}/>
-                </div>
+                            <NavLink className={Styles.cartcontainer} to='/cart'>
+                                <AiOutlineShoppingCart className={Styles.carticon}/>
+                            </NavLink>
+                        </div>
         </nav>
      );
 }
