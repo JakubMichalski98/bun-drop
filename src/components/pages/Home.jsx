e56057b5201bf3bcbdaf92ca2d8a68b825ab729f
serein3 <jakubmichalski9818@gmail.com>
2023-05-30 20:29:13 +0200

    Add products to db.json

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 
 function Home() {
+
+    const [products, setProducts] = useState([]); 
+
+    useEffect(() => {
+        async function fetchProducts() {
+            fetch(`http://localhost:3000/products`)
+            .then(res => res.json())
+            .then(data => setProducts(data));
+          }
+          fetchProducts();
+    }, [])
+
     return ( 
         <>
-            Home
+        {products.map((p) => (
+            <div>
+                <h1>{p.name}</h1>
+                <img src={p.image}/>
+            </div>
+        ))}
+
         </>
      );
 }
