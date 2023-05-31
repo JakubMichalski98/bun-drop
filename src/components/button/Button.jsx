f13a2ba9deb9b7f550668d6aba5f4cb49c2c0c69
serein3 <jakubmichalski9818@gmail.com>
2023-05-31 15:06:17 +0200

    Responsive Home Page

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Styles from './Button.module.css'
 
-function Button() {
+function Button({text, onClick}) {
+
+    function handleClick() {
+        if (onClick)
+        {
+            onClick();
+        }
+    }
+
     return ( 
-        <>
-        
-        </>
+        <div className={Styles.button}>
+            {text}
+        </div>
      );
 }
 
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import Styles from './Hero.module.css';
+import Button from '../button/Button';
+import { Link } from 'react-router-dom';
 
 function Hero() {
+
     return ( 
         <div className={Styles.container}>
             <div className={Styles.text}>
                 <h1>Drop it like it's hot!</h1>
                 <p>Whether you're looking for a quick bite or a full meal, we've got you covered. So why wait? Order now and experience the future of fast food delivery!
                 </p>
+                    <div className={Styles.desktopbuttoncontainer}>
+                        <Link to='/menu'>
+                            <Button text={'ORDER NOW'}/>
+                        </Link>
+                    </div>
             </div>
         
             <div className={Styles.imagecontainer}>
                 <img className={Styles.heroimage} src='src\images\burger-6.png'/>
             </div>
+
+            <div className={Styles.mobilebuttoncontainer}>
+                <Link to='/menu'>
+                    <Button text={'ORDER NOW'}/>
+                </Link>
+            </div>
+
 
         </div>
      );
